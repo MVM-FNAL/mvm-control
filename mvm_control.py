89fafbcee3d871aafbf3e69a0baa7cd3176e190c
Bryerton Shaw <bryerton@triumf.ca>
2020-04-17 01:21:50 -0700

    - added ability to configure ESP32 to match previous logs ESP32 config via --use-log=<logfile> - added console log command (logs via 'set console 1' option) - added stubs for load/save commands - some name cleanup up 'flux' -> 'flow' - changed timeout from 2 seconds to 0.5 - some minor code refactor - added tox, currently just for flake8 lint check

--- mvm_control.py
+++ mvm_control.py
@@ -25,7 +25,7 @@ Each 'data' entry contains the contents of the 'get all' command, and and an
 extra parameter 'time' that is a unix timestamp of when it was received.
 """
 
-__version__ = '1.0.5'
+__version__ = '1.1.0'
 
 import serial
 import time
@@ -59,6 +59,14 @@ choices_for_set = [
     'backup_min_rate', 'stats_clear'
 ]
 
+# Settings to store when starting a log file
+settings_to_store = [
+    'backup', 'power_mode', 'battery', 'version', 'alarm', 'warning',
+    'run', 'mode', 'rate', 'ratio', 'assist_ptrigger',
+    'assist_flow_min', 'ptarget', 'pressure_support', 'backup_enable',
+    'backup_min_rate'
+]
+
 # Verbosity option
 verbose = False
 rate = 10
@@ -72,6 +80,34 @@ def signal_handling(signum, frame):
 
 
 signal.signal(signal.SIGINT, signal_handling)
+
+
+def set_log_settings(args):
+    try:
+        with open(args.use_log, "r") as prev_log_file:
+            prev_log = json.load(prev_log_file)
+    except Exception as e:
+        print(e)
+        exit(-1)
+
+    settings_to_load = prev_log["settings"]
+    for setting in settings_to_load:
+        # Write all settable settings back to ESP32
+        if setting in choices_for_set:
+            if args.logfile is not sys.stdout:
+                print("Setting " + setting + " to " +
+                      str(settings_to_load[setting]))
+            set_mvm_param(ser, setting, settings_to_load[setting])
+
+
+def get_log_settings(settings_to_store):
+    settings_resp = {}
+    for setting in settings_to_store:
+        resp = get_mvm_param(ser, setting)
+        if(resp is not False):
+            settings_resp[setting] = resp
+
+    return settings_resp
 
 
 def _parse_response(response):
@@ -116,7 +152,18 @@ def set_mvm_param(ser, param, value):
     try:
         request = 'set ' + str(param) + ' ' + str(value) + '\r\n'
         ser.write(request.encode('utf-8'))
-        response = _parse_response(ser.readline())
+        # Ugly hack to deal with the buffer filled with console logs
+        if(param == "console" and int(value) == 0):
+            while True:
+                response = ser.read_until().strip().lower()
+                if(response == "valore=ok"):
+                    response = _parse_response(response)
+                    break
+                elif(response is None):
+                    break
+                time.sleep(0.1)
+        else:
+            response = _parse_response(ser.readline())
     except Exception as e:
         print("Error communicating with device")
         print(e)
@@ -163,77 +210,156 @@ def cmd_log(args):
     global ser
     global rate
     global verbose
-    with args.logfile as log_file:
-        # Step one, gather up the settings!
-        settings = [
-           'backup', 'power_mode', 'battery', 'version', 'alarm', 'warning',
-           'run', 'mode', 'rate', 'ratio', 'assist_ptrigger',
-           'assist_flow_min', 'ptarget', 'pressure_support', 'backup_enable',
-           'backup_min_rate'
-        ]
 
-        # Run through the dict getting our responses
-        settings_resp = {}
-        for setting in settings:
-            resp = get_mvm_param(ser, setting)
-            if(resp is not False):
-                settings_resp[setting] = resp
+    # Disable verbose if logging to stdout
+    if(args.logfile is not sys.stdout and verbose):
+        verbose = True
+    else:
+        verbose = False
 
-        # Dump out the settings to the log file
-        log_file.write("{\"settings\": " + json.dumps(settings_resp))
+    # Should we configure the ESP32 with settings from a previous logfile?
+    if(args.use_log is not None):
+        set_log_settings(args)
 
-        # Start the data portion of the log file
+    # Run through the dict getting our responses
+    settings_resp = get_log_settings(settings_to_store)
 
-        log_file.write(",\"data\": [\n")
-        first = True  # Used to print the ',' between entries
+    # Dump out the settings to the log file
+    args.logfile.write("{\"settings\": " + json.dumps(settings_resp))
 
-        while(1):
-            resp = get_mvm_param(ser, 'all')
-            if(resp is False):
-                exit(-1)
+    # Start the data portion of the log file
 
-            # Don't print the data if we are logging to standard out or not in
-            # verbose mode
-            if(log_file is not sys.stdout and verbose):
-                print(resp)
+    args.logfile.write(",\"data\": [\n")
+    first = True  # Used to print the ',' between entries
 
-            # Ugly, but split up the data and use some sort of name that makes
-            # sense the variable names used inside the Arduino code are a bit
-            # hard to parse/grok
-            data_split = resp.split(',')
-            log_file.write(
-                '{13}{{"time": {14}, "last_pressure": {0}, '
-                '"flux": {1}, "last_o2": {2}, "last_bpm": {3}, '
-                '"tidalvolume": {4}, "last_peep": {5}, "temperature": {6}, '
-                '"battery_powered": {7}, "current_battery_charge": {8}, '
-                '"current_p_peak": {9}, "current_t_visnp": {10}, '
-                '"current_t_vesp": {11}, "current_vm": {12}}}\n'.format(
-                    data_split[0],
-                    data_split[1],
-                    data_split[2],
-                    data_split[3],
-                    data_split[4],
-                    data_split[5],
-                    data_split[6],
-                    data_split[7],
-                    data_split[8],
-                    data_split[9],
-                    data_split[10],
-                    data_split[11],
-                    data_split[12],
-                    '' if first else ',',
-                    float(time.time())))
-            if(first):
-                first = False
+    while(1):
+        resp = get_mvm_param(ser, 'all')
+        if(resp is False):
+            exit(-1)
 
-            if terminate:
-                log_file.write("]}\n")
-                log_file.close()
-                exit(0)
+        if(verbose):
+            print(resp)
 
-            time.sleep(rate)
+        # Ugly, but split up the data and use some sort of name that makes
+        # sense the variable names used inside the Arduino code are a bit
+        # hard to parse/grok
+        data_split = resp.split(',')
+        args.logfile.write(
+            '{13}{{"time": {14}, "last_pressure": {0}, '
+            '"last_flow": {1}, "last_o2": {2}, "last_bpm": {3}, '
+            '"tidal_volume": {4}, "last_peep": {5}, "temperature": {6}, '
+            '"battery_powered": {7}, "current_battery_charge": {8}, '
+            '"current_p_peak": {9}, "current_t_visnp": {10}, '
+            '"current_t_vesp": {11}, "current_vm": {12}}}\n'.format(
+                data_split[0],
+                data_split[1],
+                data_split[2],
+                data_split[3],
+                data_split[4],
+                data_split[5],
+                data_split[6],
+                data_split[7],
+                data_split[8],
+                data_split[9],
+                data_split[10],
+                data_split[11],
+                data_split[12],
+                '' if first else ',',
+                float(time.time())))
+        if(first):
+            first = False
 
-    exit(0)
+        if terminate:
+            args.logfile.write("]}\n")
+            args.logfile.close()
+            exit(0)
+
+        time.sleep(rate)
+
+
+def cmd_console_log(args):
+    """Console Log command wrapper"""
+    global terminate
+    global ser
+    global verbose
+
+    # Disable verbose if logging to stdout
+    if(args.logfile is not sys.stdout and verbose):
+        verbose = True
+    else:
+        verbose = False
+
+    # Should we configure the ESP32 with settings from a previous logfile?
+    if(args.use_log is not None):
+        set_log_settings(args)
+
+    # Run through the dict getting our responses
+    settings_resp = get_log_settings(settings_to_store)
+
+    # Dump out the settings to the log file
+    args.logfile.write("{\"settings\": " + json.dumps(settings_resp))
+
+    # Start the data portion of the log file
+    args.logfile.write(",\"data\": [\n")
+    first = True  # Used to print the ',' between entries
+
+    # Start up console logger
+    resp = set_mvm_param(ser, 'console', 1)
+    if(resp is False):
+        exit(-1)
+
+    if(verbose):
+        print(resp)
+
+    while(1):
+
+        # Ugly, but split up the data and use some sort of name that makes
+        # sense the variable names used inside the Arduino code are a bit
+        # hard to parse/grok
+        resp = ser.readline().strip()
+        if(verbose):
+            print(resp)
+        data_split = resp.split(',')
+        args.logfile.write(
+            '{11}{{"time": {12}, "ts": {0}, '
+            '"last_flow": {1}, "last_pressure0": {2}, "last_pressure1": {3}, '
+            '"pid_monitor": {4}, "pid_monitor2": {5}, "valve2_status": {6}, '
+            '"venturi_flux": {7}, "flow": {8}, '
+            '"tidal_volume": {9}, "dgb_delta": {10}}}\n'.format(
+                data_split[0],
+                data_split[1],
+                data_split[2],
+                data_split[3],
+                data_split[4],
+                data_split[5],
+                data_split[6],
+                data_split[7],
+                data_split[8],
+                data_split[9],
+                data_split[10],
+                '' if first else ',',
+                float(time.time())))
+
+        if(first):
+            first = False
+
+        if terminate:
+            # Turn off console logging
+            set_mvm_param(ser, 'console', 0)
+            args.logfile.write("]}\n")
+            args.logfile.close()
+            exit(0)
+
+        # Brief sleep so this script doesn't lock up CPU
+        time.sleep(0.005)
+
+
+def cmd_load(args):
+    pass
+
+
+def cmd_save(args):
+    pass
 
 
 def main():
@@ -275,8 +401,32 @@ def main():
         parser_set.add_argument("value")
         parser_set.set_defaults(func=cmd_set)
 
+        # Load configuration command
+        parser_load = subparsers.add_parser(
+            'load', help="load configuration into ESP32")
+        parser_load.add_argument(
+            'cfgfile',
+            nargs='?',
+            type=argparse.FileType('r'),
+            default=sys.stdin,
+            help="Optional, leave blank for stdin")
+        parser_load.set_defaults(func=cmd_load)
+
+        # Save configuration command
+        parser_save = subparsers.add_parser(
+            'save', help="save configuration from ESP32")
+        parser_save.add_argument(
+            'cfgfile',
+            nargs='?',
+            type=argparse.FileType('w+'),
+            default=sys.stdout,
+            help="Optional, leave blank for stdout")
+        parser_save.set_defaults(func=cmd_save)
+
         # Log command
         parser_log = subparsers.add_parser('log', help="log <file>")
+        parser_log.add_argument(
+            '-u', '--use-log', help="Use previous log file to setup ESP32")
         parser_log.add_argument(
             'logfile',
             nargs='?',
@@ -284,6 +434,18 @@ def main():
             default=sys.stdout,
             help="Optional, leave blank for stdout")
         parser_log.set_defaults(func=cmd_log)
+
+        # 'Console' Log command
+        parser_clog = subparsers.add_parser('console_log', help="log <file>")
+        parser_clog.add_argument(
+            '-u', '--use-log', help="Use previous log file to setup ESP32")
+        parser_clog.add_argument(
+            'logfile',
+            nargs='?',
+            type=argparse.FileType('w+'),
+            default=sys.stdout,
+            help="Optional, leave blank for stdout")
+        parser_clog.set_defaults(func=cmd_console_log)
 
         args = parser.parse_args()
         verbose = args.verbose
@@ -304,7 +466,7 @@ def main():
             bytesize=8,
             parity='N',
             stopbits=1,
-            timeout=2)
+            timeout=0.5)
     except Exception:
         print("Failed to connect to serial port " + args.port)
         exit(-1)
